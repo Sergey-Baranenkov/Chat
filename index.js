e15a0a2607c93b4895be7606a9dda62d5753e462
Sergey <baranenkovs@mail.ru>
2019-06-06 17:21:25 +0300

    tethered with a database

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
-var PORT = process.env.PORT||5000;
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const PORT = process.env.PORT||5000;
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const MongoClient = require('mongodb').MongoClient;
 
 app.use('/assets', express.static('assets'))
+
+const url='mongodb://localhost:27017/node_chat';
+
+MongoClient.connect(url, function(err,db){
+  const database = db.db('node_chat');
+  const messagesCollection = database.collection('messages');
+  io.on('connection', function(socket){
+    console.log('a user connected');
+
+    messagesCollection.find().toArray().then((docs)=>{
+      socket.emit("chatHistory",docs);
+    });
+
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg){
+      messagesCollection.insertOne({text:msg,function(err,res){
+        console.log("Inserted");
+      }});
+      io.emit('chat message', msg);
+    });
+  });
+})
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
 
 http.listen(PORT, function(){
-  console.log('listening on *:3000');
+  console.log('listening on *:5000');
 });
