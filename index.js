398287d21e1fe39acda76d80ddd7d26b901b30c5
Sergey <baranenkovs@mail.ru>
2019-06-20 14:36:02 +0300

    interface was redesigned for nxt updates,list of online users was added.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const MongoClient = require('mongodb').MongoClient;
 const Canvas = require('canvas')
-
+const crypto = require('crypto');
 app.use('/assets', express.static('assets'))
 
 const url='mongodb://localhost:27017/node_chat';
@@ -14,17 +14,46 @@ MongoClient.connect(url, function(err,db){
   const database = db.db('node_chat');
   const messagesCollection = database.collection('messages');
   const usersCollection = database.collection('users');
+  const onlineCollection=database.collection('online');
+
   io.on('connection', function(socket){
-    console.log('a user connected');
+    console.log('connect '+socket.id);
+
     socket.on('disconnect', function(){
-      console.log('user disconnected');
+      if (socket._login){
+        usersCollection.findOne({'login':socket._login}).then((has)=>{
+          const reducedTab=has.Tab-1;
+          usersCollection.updateOne({'login':socket._login}, {$set: {Tab:reducedTab}});
+          if(reducedTab==0){
+            io.emit("UserDisconnected",socket._login);
+            onlineCollection.remove({user:socket._login});
+          }
+        });
+      }
     });
 
     socket.on('login form',function(form,option){
         if(form.login!=""&&form.password!=""){
         usersCollection.findOne({'login':form.login,'password':form.password}).then((has)=>{
             if(has!=null){
+              const newUserUpdate=async ()=>{
+                if(+has.Tab==0){
+                  await function(){
+                    onlineCollection.insertOne({user:form.login})
+                    io.emit('newOnlineUser',form.login);
+                  }();
+                }
+                await function(){
+                  onlineCollection.find().toArray().then((docs)=>{
+                    socket.emit("onlineHistory",docs);
+                  });
+                }();
+              }
+              newUserUpdate();
+
+              usersCollection.updateOne({'login':form.login}, {$set: {Tab:has.Tab+1}});
               socket.emit("Successful validation",option);
+              socket._login=form.login;
               socket.on('chat message', function(msg){
                 if(msg!=""){
                   messagesCollection.insertOne({text:form.login + ': ' + msg});
@@ -36,10 +65,10 @@ MongoClient.connect(url, function(err,db){
               });
             }else if (option=="ls"){
               socket.emit('needToL/R');
-            }else{
-              socket.emit('error log',"Invalid login-password combination");
             }
         });
+      }else{
+        socket.emit('error log',"Invalid login-password combination");
       }
     });
 
@@ -80,8 +109,19 @@ MongoClient.connect(url, function(err,db){
               if(userCaptcha==code){
                  usersCollection.insertOne({login:form.login,
                                             password:form.password,
-                                            Email:form.Email});
+                                            Email:form.Email,
+                                            Tab:0});
                 socket.emit("Successful validation");
+
+
+                onlineCollection.insertOne({user:form.login});
+                socket.broadcast.emit('newOnlineUser',form.login);
+
+                onlineCollection.find().toArray().then((docs)=>{
+                  socket.emit("onlineHistory",docs);
+                });
+
+
                 socket.on('chat message', function(msg){
                   if(msg!=""){
                     messagesCollection.insertOne({text:form.login + ': ' + msg});
